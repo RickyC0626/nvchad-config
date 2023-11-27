d55f176be6ad7227a5ba987caeae5e23a86d72ae
Ricky Chon <rickychon99@gmail.com>
2023-11-27 18:34:22 -0500

    Show gitignored files & opened files in nvim-tree

--- lua/plugins/configs/nvimtree.lua
+++ lua/plugins/configs/nvimtree.lua
@@ -20,7 +20,7 @@ local options = {
   },
   git = {
     enable = true,
-    ignore = true,
+    ignore = false,
   },
   filesystem_watchers = {
     enable = true,
@@ -32,8 +32,8 @@ local options = {
   },
   renderer = {
     root_folder_label = false,
-    highlight_git = false,
-    highlight_opened_files = "none",
+    highlight_git = true,
+    highlight_opened_files = "name",
 
     indent_markers = {
       enable = false,
